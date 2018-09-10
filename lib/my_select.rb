0d0882d3d8c896de9424120521f34e5360262339
Sang Song <sangyoon.song@gmail.com>
2018-09-10 11:59:16 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
-def my_select(collection)
- # your code here!
+def my_select(arg)
+  i = 0
+  arr = []
+  
+  while i < arg.length do
+    if yield(arg[i])
+      arr << arg[i]
+    end
+    i+=1
+  end
+  arr
 end
