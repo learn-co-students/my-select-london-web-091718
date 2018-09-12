8670b9c10ebfd45acb0f81f1a58b403ebd434a8d
patrickp246@gmail.com <patrickp246@gmail.com>
2018-09-12 18:27:10 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+ counter = 0
+ rtn_array = []
+ if collection.count != 0 
+   while counter < collection.count do
+     if yield(collection[counter])
+       rtn_array << collection[counter]
+     end
+     counter+=1
+   end
+ end
+ rtn_array
 end
