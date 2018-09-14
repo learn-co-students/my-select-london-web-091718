674b8271c4b4351786a1f84cd9606e5573350fca
Barney Loosemore <flatiron@barney.co.uk>
2018-09-14 16:29:53 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,9 @@
 def my_select(collection) 
   i = 0 
   array = []
-  if collection.length = 0 
-    exit
-  while i < collection.length 
-    if yield(collection[i])
+  if collection.length != 0  
+    while i < collection.length 
+      if yield(collection[i])
       array << collection[i]
     end
     i += 1 
